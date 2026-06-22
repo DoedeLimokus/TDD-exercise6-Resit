80991edacedb3890f8601be5347a19656a9ebdf1
DoedeLimokus <doede@buchung.nl>
2026-06-22 12:05:45 +0200

    added: extra tests to giveLivingCells

--- gameOfLife.test.mjs
+++ gameOfLife.test.mjs
@@ -66,4 +66,10 @@ describe("Determine wich cells should be living", () => {
     it("Return a list of coords of living cells 1", () => {
         expect(giveLivingCells([[0,0,1,0], [1,0,1,1], [2,0,2,0], [0,1,3,0], [1,1,5,0], [2,1,3,1], [0,2,1,1], [1,2,3,1], [2,2,2,1]])).toEqual([[0,1],[2,1],[1,2],[2,2]])
     })
+    it("Return a list of coords of living cells 2", () => {
+        expect(giveLivingCells([[0,0,1,0], [1,0,2,1], [2,0,2,1], [0,1,2,0], [1,1,5,0], [2,1,3,1], [0,2,0,1], [1,2,3,0], [2,2,1,1]])).toEqual([[1,0],[2,0],[2,1], [1,2]])
+    })
+    it("Return a list of coords of living cells 3", () => {
+        expect(giveLivingCells([[0,0,1,0], [1,0,2,1], [2,0,3,1], [3,0,3,0], [0,1,2,0], [1,1,4,0], [2,1,5,1], [3,1,3,1], [0,2,1,0], [1,2,1,1], [2,2,4,0], [3,2,2,1]])).toEqual([[1,0], [2,0], [3,0], [3,1], [3,2]])
+    })
 })
